28db6e64978c43f8d9cb3eb15f3c4fa7f6d08710
venith-creator <patrickaghidi222005@gmail.com>
2025-06-26 13:33:17 -0700

    final

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&i$3i1l0cv3t&c&v@o5nux&dt$7vhc^&$i^4vx!w7j5(zdnzx!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

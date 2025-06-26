0534273e40d4e4f208377b93ceb88cbb0f82b8dd
venith-creator <patrickaghidi222005@gmail.com>
2025-06-26 14:10:36 -0700

    finall

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,11 +74,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
